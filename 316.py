a0d72f4548e2ed4e97bc4fa8933c9913cab3cb27
c110156127 <c110156127@nkust.edu.tw>
2022-03-22 11:45:13 +0800

    上課

--- 316.py
+++ 316.py
@@ -1,1 +1,19 @@
-print("erewrqwrqweqw")
+import random
+tmp = [1,2,3]
+aaa = (1,2,3)
+tmp[0]=2
+
+k = "c1101561"
+d = []
+for i in range(1,60):
+    d.append(k+'{0:02d}'.format(i))
+
+
+grade = []
+for i in range(1,60):
+    grade.append(random.randint(0,100))
+
+#dict
+tmp2 = {"c110156101":88,"c110156102":78,"c110156103":90}
+print(grade[0])
+print(tmp2["c110156103"])
